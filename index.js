8e402f204a9521fa469553e3f373d2aadd923871
Liam Argent <liargent@hotmail.com>
2018-07-18 19:57:39 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+  
+}
+
+function subtract(a, b) {
+  
+}
+
+function multiply(a, b) {
+  
+}
+
+function divide(a, b) {
+  
+}
+
+function increment(a){
+  
+}
+
+function decrement(a) {
+  
+}
